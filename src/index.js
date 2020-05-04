c5698515f65aa97369111d8d8d6d04b79e190ccc
beatorizu <betrizaf@gmail.com>
2020-05-04 14:11:15 -0300

    feat: Get combo promotion

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
+function getComboPromotion(looks) {
+	if (looks.length == 1) {
+		return 'SINGLE LOOK';
+	}
+	if (looks.length == 2) {
+		return 'DOUBLE LOOK';
+	}
+	if (looks.length == 3) {
+		return 'TRIPLE LOOK';
+	}
+	return 'FULL LOOK';
+}
+
 function getShoppingCart(ids, productsList) {
 	const products = productsList.filter((product) => {
 		return ids.includes(product.id);
 	});
+	let promotion = getComboPromotion(Array.from(new Set(products.map(product => product.category))));
 
 	return {
 		products: products.map((product) => {
 			return { name: product.name, category: product.category };
 		}),
-		promotion: "",
-		totalPrice: "",
-		discountValue: "",
-		discount: "",
+		promotion: promotion,
+		totalPrice: '',
+		discountValue: '',
+		discount: '',
 	};
 }
 
