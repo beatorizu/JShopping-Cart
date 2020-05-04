540abcd23dc39a8c5037cdef2998c77a5d6815a8
beatorizu <betrizaf@gmail.com>
2020-05-04 14:05:08 -0300

    feat: Show name and category in the shopping cart

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
 function getShoppingCart(ids, productsList) {
+	const products = productsList.filter((product) => {
+		return ids.includes(product.id);
+	});
+
 	return {
-		products: [],
+		products: products.map((product) => {
+			return { name: product.name, category: product.category };
+		}),
 		promotion: "",
 		totalPrice: "",
 		discountValue: "",
