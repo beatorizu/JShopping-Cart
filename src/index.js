78627d83b3d4632ce13e5b779a7ed86689689926
beatorizu <betrizaf@gmail.com>
2020-05-04 16:10:40 -0300

    refactor: Select combo promotion using promotion array

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
-function getComboPromotion(looks) {
-	if (looks.length == 1) {
-		return 'SINGLE LOOK';
-	}
-	if (looks.length == 2) {
-		return 'DOUBLE LOOK';
-	}
-	if (looks.length == 3) {
-		return 'TRIPLE LOOK';
-	}
-	return 'FULL LOOK';
-}
-
 function getComboPromotionPrice(product, comboPromotion) {
 	let promotion = product.promotions.filter((promotion) =>
 		promotion.looks.includes(comboPromotion)
@@ -27,7 +14,10 @@ function getShoppingCart(ids, productsList) {
 	const products = productsList.filter((product) => {
 		return ids.includes(product.id);
 	});
-	let comboPromotion = getComboPromotion(Array.from(new Set(products.map(product => product.category))));
+	let comboPromotion =
+		promotions[
+			Array.from(new Set(products.map((product) => product.category))).length - 1
+		];
 	let regularPrice = products.reduce((acc, product) => acc + product.regularPrice, 0);
 	let totalPrice = products.reduce((acc, product) => acc + getComboPromotionPrice(product, comboPromotion), 0);
 
